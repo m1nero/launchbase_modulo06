6c3cb3e6343fa8978e2d8dc5baeb9175885cbb30
Victor <victor.fernandes01@hotmail.com>
2020-07-24 13:37:39 -0300

    Create e fields

--- ReadMe.txt
+++
@@ -1,6 +0,0 @@
-Create
-Read
-Update
-Delete
-
-Site para cadastro de alunos e instrutores academia + Banco de Dados PostgreSQL
--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const nunjucks = require('nunjucks');
-const routes = require("./routes");
-const methodOverride = require('method-override');
+const express = require('express')
+const nunjucks = require('nunjucks')
+const routes = require("./routes")
+const methodOverride = require('method-override')
 
-const server = express();
+const server = express()
 
-server.use(express.urlencoded ({extended : true}));
-server.use(express.static('public'));
-server.use(methodOverride('_method'));
-server.use(routes);
+server.use(express.urlencoded ({extended : true}))
+server.use(express.static('public'))
+server.use(methodOverride('_method'))
+server.use(routes)
 
-server.set("view engine", "njk");
+server.set("view engine", "njk")
 
 nunjucks.configure("src/app/views", {
     express: server,
@@ -19,5 +19,5 @@ nunjucks.configure("src/app/views", {
 })
 
 server.listen(5000, function() {
-    console.log("server is running");
+    console.log("server is running")
 })
