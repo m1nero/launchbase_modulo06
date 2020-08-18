1b483c46f4dfac8c88a8868788a55cdca12c4298
Victor Monteiro <50376581+m1nero@users.noreply.github.com>
2020-08-18 11:24:58 -0300

    Filtros de busca

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -4,7 +4,7 @@ module.exports = {
     all() {
         return db.query(`SELECT * FROM products ORDER BY updated_at DESC`)
     },
-    
+
     create(data) {
         const query = `INSERT INTO products (
             category_id,
@@ -69,5 +69,27 @@ module.exports = {
 
     files(id) {
         return db.query(` SELECT * FROM files WHERE product_id = $1`, [id])
+    },
+
+    search(params) {
+        const {filter, category} = params
+
+        let query = "",
+            filterQuery = `WHERE`
+
+        if(category) {
+            filterQuery = `${filterQuery}
+            products.category_id = ${category}
+            AND`
+        }
+
+        filterQuery = `${filterQuery} products.name ILIKE '%${filter}%'
+        OR products.description ILIKE '%${filter}%'`
+
+        query = `SELECT products.*, categories.name AS category_name
+            FROM products LEFT JOIN categories ON (categories.id = products.category_id)
+            ${filterQuery}`
+        
+        return db.query(query)
     }
 }
